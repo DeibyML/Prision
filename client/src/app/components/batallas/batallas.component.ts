478dd8c6ef109ffc5205ea6cfc7eb160b3c40252
eewsa <30420073+eewsa@users.noreply.github.com>
2019-02-21 20:50:36 -0500

    Módulo de batalla finalizado

--- client/src/app/components/batallas/batallas.component.ts
+++ client/src/app/components/batallas/batallas.component.ts
@@ -9,10 +9,18 @@ declare var M;
 })
 export class BatallasComponent implements OnInit {
 
-  luchador1 = '';
-  luchador2 = '';
+  luchador1: any = '';
+  luchador2: any = '';
+  arma1 = '';
+  arma2 = '';
+  ganador: any = {};
+  ambiente = '';
+  luchando = false;
   prisioneros: Prisionero[] = [];
   prisionerosAll: Prisionero[] = [];
+  armas: string[] = ['La motosierra de Uribe', 'Bayesta lanza huevos',
+                    'Pistola de agua', 'Arma de rayo laser', 'El Guantele', 'Cauchera', 'Espada Samurai'];
+  ambientes: string[] = ['Miel de oveja', 'Aceite de ballena', 'Piscina de orines', 'Lodo', 'Ácido sulfúrico'];
 
   constructor(private prisioneroService: PrisioneroService) { }
 
@@ -44,6 +52,49 @@ export class BatallasComponent implements OnInit {
 
   setLuchador2(luchador) {
     this.luchador2 = luchador;
+  }
+
+  setArma1(arma) {
+    this.arma1 = arma;
+  }
+
+  setArma2(arma) {
+    this.arma2 = arma;
+  }
+
+  setAmbiente(ambiente) {
+    this.ambiente = ambiente;
+  }
+
+  luchar() {
+    this.luchando = true;
+    // this.ganador.arma;
+    console.log(this.luchador1);
+    console.log(this.luchador2);
+
+    let fecha1 = new Date(this.luchador1.FechaNac);
+    let fecha2 = new Date(this.luchador2.FechaNac);
+
+    if (fecha1 > fecha2) {
+      this.ganador.arma = this.arma1;
+      this.ganador.nombre = this.luchador1.Nombres + ' ' + this.luchador1.Apellidos;
+    } else {
+      this.ganador.arma = this.arma2;
+      this.ganador.nombre = this.luchador2.Nombres + ' ' + this.luchador2.Apellidos;
+    }
+
+    setTimeout(() => {
+      this.luchando = false;
+      M.toast({html: 'El ganador en del encuentro fue ' + this.ganador.nombre + ' con su arma ' + this.ganador.arma,
+               classes: 'rounded',
+               options: { displayLength: 6500}});
+
+      setTimeout(() => {
+      M.toast({html: '¡El escenario está listo para una nueva batalla!', classes: 'rounded'});
+      location.reload();
+      }, 2500);
+
+    }, 3500);
   }
 
 }
